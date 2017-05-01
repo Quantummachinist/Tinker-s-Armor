448f58638e096c3febba082502cc5bbe56f38d0f
ethan walker <ethan walker>
2017-04-30 19:13:26 -0500

    made the mod load when lanched

---
+++ src/main/java/com/Qunatummachinist/TinkersArmor/TinkersArmor.java
@@ -0,0 +1,29 @@
+package com.Qunatummachinist.TinkersArmor;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="TinkersArmor", name="Tinker's Armor", version="1.7.10-1.0")
+public class TinkersArmor {
+    @Mod.Instance("TinkersArmor")
+    public static TinkersArmor instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+
+
+}
